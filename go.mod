554c012d0107fade1806bb29380112c4a76adb04
pefish <pefish@qq.com>
2020-07-03 15:24:13 +0800

    upgrade

--- go.mod
+++ go.mod
@@ -2,12 +2,12 @@ module github.com/pefish/storm-golang-sdk
 
 require (
 	github.com/btcsuite/btcd v0.0.0-20190213025234-306aecffea32
-	github.com/pefish/go-decimal v0.2.0
-	github.com/pefish/go-error v0.3.4
+	github.com/pefish/go-decimal v0.2.2
+	github.com/pefish/go-error v0.3.5
 	github.com/pefish/go-format v0.1.3
-	github.com/pefish/go-http v0.4.6
-	github.com/pefish/go-json v0.1.2
-	github.com/pefish/go-reflect v0.2.0
+	github.com/pefish/go-http v0.4.9
+	github.com/pefish/go-json v0.1.6
+	github.com/pefish/go-reflect v0.2.1
 )
 
 go 1.13
--- signature/key_test.go
+++ signature/key_test.go
@@ -11,12 +11,14 @@ func TestGenerateRandomKeyPair(t *testing.T) {
 
 func TestSignMessage(t *testing.T) {
 	//pubKey := `0302e2b8b44fe202ac68f2f74a8a503496fbec3d9c3b686b9f06812e08d9fbbd78`
-	privKey := `1b75f9d2651786bbc3da124f2f52fb853ad7f4abadf1c2fac545f0b68d4d29d9`
-	fmt.Println(SignMessage(`haha`, privKey))
+	privKey := `60a10883c8252658bed264951385a62096d69f5ce4fd9e43b690f091e29ab2bb`
+	fmt.Println(SignMessage(`{"amount":"14098.3268000000","chain":"Pmeer","confirmation":3,"confirmation_threshold":15,"created_at":"2020-06-09T03:35:21.000Z","currency":"PMEER","decimals":8,"height":null,"network_fee":"0.0010000000","network_fee_currency":"PMEER","network_fee_decimal":8,"request_id":"574-","side":"withdraw","to_address":"TmYXzyTiNKrK6mQG8qkxdJeXiE55xrbhYgu","tx_id":"6e8d378ae7f199247255f28f41531b4fdbae797c1f8eee1935eb1383daf819e0","user_id":3}`, privKey))
 }
 
 func TestVerifySignature(t *testing.T) {
 	pubKey := `0302e2b8b44fe202ac68f2f74a8a503496fbec3d9c3b686b9f06812e08d9fbbd78`
-	sig := `3045022100f063bba67553d77f486eac8d89867c5a6d7e61a11e432bc1c71a4f114daabeb902206a88287f43cca1092fffbc2970b0ba15695d672e678dc66eae90c49c9c8c70b5`
-	fmt.Println(VerifySignature(`haha`, sig, pubKey))
+	sig := `304402202e3e0b1ecf96c3d190c9cebc20af9fcaa3f006da0fde34f64548e78134cb80bd0220027d314829124c0d88d3c3bc60feca357308ae7d566792c3c5ae993e7a4b33c8`
+	if VerifySignature(`{"amount":232.95032569,"block_id":"0000040102e563e8878c0f6d9ca228e5a70a7be993b41a00af55abe73530d6e8","chain":"Pmeer","confirmation":100,"confirmation_threshold":10,"created_at":"2020-06-08T08:30:42Z","currency":"PMEER","decimals":8,"from_address":"","height":"463184","network_fee":0,"network_fee_currency":"PMEER","network_fee_decimal":8,"output_index":5,"request_id":"","side":"deposit","tag":"","to_address":"TmgaeDCBR9sHVXYbyTm4HwUX7nyUsNtsDJz","tx_id":"874cfe640ba987b9bcaffcc37e03d243e576feb4fb6397a4644742942c273561","user_id":3}`, sig, pubKey) != true {
+		t.Error()
+	}
 }
